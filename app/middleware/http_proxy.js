824341564cfda64d1ff87fbcd2b02d7f49dde46a
yunwisdom <qq695926920@163.com>
2021-02-02 20:44:17 +0800

    init

--- app/middleware/http_proxy.js
+++ app/middleware/http_proxy.js
@@ -4,27 +4,69 @@ const proxy = require('http-proxy-middleware');
 const c2k = require('koa2-connect');
 const pathMatching = require('egg-path-matching');
 const omit = require('lodash/omit');
+const { tar } = require('compressing');
+const nacos = require('nacos');
 
-module.exports = options => {
-  return async function httpProxy(ctx, next) {
-    const proxyTable = omit(options, [ 'enable', 'match', 'ignore' ]);
-    const path = ctx.request.originalUrl || ctx.request.url;
+let client = null;
+let serviceMap = {};
 
-    Object.keys(proxyTable).some(context => {
-      const match = pathMatching({ match: context });
-      const isMatch = match({ path });
+module.exports = (options) => {
 
-      if (isMatch) {
-        let proxyOptions = proxyTable[context];
-        if (typeof proxyOptions === 'string') {
-          proxyOptions = { target: proxyOptions };
-        }
-        c2k(proxy(context, proxyOptions))(ctx, next);
-      }
+    const nacosConfig = options['@nacos'];
+    console.log(JSON.stringify(nacosConfig));
 
-      return isMatch;
-    });
+    if (!client) { //如果nacos客户端尚未初始化，则初始化并建立连接
+        client = new nacos.NacosNamingClient(nacosConfig);
+        client.ready().then(() => {
+            Object.keys(options).some(context => {
+                const proxyTableConfig = JSON.parse(JSON.stringify(options));
+                let proxyOptions = proxyTableConfig[context];
+                if (proxyOptions.serviceName) {
+                    client.subscribe(proxyOptions.serviceName, hosts => {
+                        serviceMap[proxyOptions.serviceName] = hosts;
+                        console.log(`hosts:` + JSON.stringify(hosts));
+                    });
+                }
+            });
+        })
+    }
 
-    await next();
-  };
+    return async function httpProxy(ctx, next) {
+        const proxyTable = omit(options, ['enable', 'match', 'ignore']);
+        const path = ctx.request.originalUrl || ctx.request.url;
+        console.log('web proxy path: ' + path + ' proxy table: ' + JSON.stringify(proxyTable));
+        Object.keys(proxyTable).some(context => {
+            const proxyTableConfig = JSON.parse(JSON.stringify(proxyTable));
+            const match = pathMatching({ match: context });
+            const isMatch = match({ path });
+            if (isMatch) {
+                let proxyOptions = proxyTableConfig[context];
+                if (typeof proxyOptions === 'string') {
+                    proxyOptions = { target: proxyOptions };
+                }
+                // load balance random 
+                const targets = proxyOptions.target;
+                let index = Math.ceil(Math.random() * targets.length);
+                index = index == 0 ? 0 : index - 1;
+                let target = targets[index];
+                proxyOptions.target = target;
+                // 如果存在@serviceName且存在配置注册服务，则获取serviceName对应的targets列表，并将target设置到proxyOptions中
+                if (proxyOptions.serviceName && serviceMap[proxyOptions.serviceName] && serviceMap[proxyOptions.serviceName].length > 0) {
+                    const targetList = serviceMap[proxyOptions.serviceName];
+                    let tindex = Math.ceil(Math.random() * targetList.length);
+                    tindex = tindex == 0 ? 0 : tindex - 1;
+                    let baseTarget = targetList[tindex];
+                    const baseURL = 'http://' + baseTarget.ip + ':' + baseTarget.port;
+                    proxyOptions.target = baseURL;
+                    console.log('baseURL: ' + baseURL);
+                }
+                console.log('target index :' + index + ' proxy options: ' + JSON.stringify(proxyOptions));
+                c2k(proxy(context, proxyOptions))(ctx, next);
+            }
+
+            return isMatch;
+        });
+
+        await next();
+    };
 };
